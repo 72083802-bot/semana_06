4f86008bc10cb7c6b2ae892e5adb59f24727e61f
madison <72083802@institutourusayhu.edu.pe>
2026-09-17 12:42:17 -0500

    rame- estudiante

--- semana6.py
+++ semana6.py
@@ -5,3 +5,9 @@ altura = input("Altura: ")
 print("Nombre:", nombre)
 print("Edad:", edad)
 print("Altura:", altura)
+
+semestre = input("semestre: ")
+carrera = input("carrera: ")
+
+print("semestre:", semestre)
+print("carrera:", carrera)
